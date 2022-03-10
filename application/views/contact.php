52d140e16117ea970524d954095cd3b5374d1ba2
muneeb29 <98808077+muneeb29@users.noreply.github.com>
2022-03-10 19:00:02 +0000

    Added Content To Contact Page

--- application/views/contact.php
+++ application/views/contact.php
@@ -1,16 +1,71 @@
 <html>
-	<div class="container bg-primary text-center my-3 rounded">
-		<h1>Contact Us</h1>
-	</div>
+	<div>
+		<div class="container bg-primary text-center my-3 rounded">
+			<h1>Contact Us</h1>
+		</div>
 
-	<div class="container">
-		<div class="row mx-auto">
-			<div class="col">
-				<h4>Faq's</h4>
+		<div class="container">
+			<div class="row text-center">
+				<div class="col border">
+					<h4>Faq's</h4>
+				</div>
+				<div class="col col-sm-1"></div>
+				<div class="col border">
+					<h4>Contact</h4>
+				</div>
 			</div>
-            <div class="col">
-                <h4>Contact</h4>
-            </div>
+
+			<div class="row text-center">
+				<div class="col border">How Far Do We Deliver?</div>
+				<div class="col border">Within 5 Miles Radius</div>
+				<div class="col col-sm-1"></div>
+				<div class="col border">Phone:</div>
+				<div class="col border">0161-000-0000</div>
+			</div>
+
+			<div class="row text-center">
+				<div class="col border">How Many Days A Week Are We Open?</div>
+				<div class="col border">7 Days A Week</div>
+				<div class="col col-sm-1"></div>
+				<div class="col border">Address</div>
+				<div class="col border">
+					1 Restaurant Road <br />
+					1R0 5ET <br />
+					Restaurant City <br />
+					United Kingdom
+					<br />
+				</div>
+			</div>
+
+			<div class="row text-center">
+				<div class="col border">Where Is the Restaurant Close To?</div>
+				<div class="col border">Near The Town Centre</div>
+				<div class="col col-sm-1"></div>
+				<div class="col border">Email</div>
+				<div class="col border">Restaurant@gmail.com</div>
+			</div>
+
+			<div class="row text-center">
+				<div class="col border">Is There A Car Park?</div>
+				<div class="col border">Yes We Offer Free Parking</div>
+				<div class="col col-sm-1"></div>
+				<div class="col border">Opening Hours:</div>
+				<div class="col border">
+					Monday - 9:00 - 22:00
+					<br />
+					Tuesday - 9:00 - 22:00
+					<br />
+					Wednesday - 9:00 - 22:00
+					<br />
+					Thursday - 9:00 - 22:00
+					<br />
+					Friday - 9:00 - 22:00
+					<br />
+					Saturday - 9:00 - 23:00
+					<br />
+					Sunday - 11:30 - 19:00
+				</div>
+			</div>
 		</div>
 	</div>
 </html>
