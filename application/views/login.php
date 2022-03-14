4ab8915ea0b425f6a528084c2460deac6a20b223
muneeb29 <98808077+muneeb29@users.noreply.github.com>
2022-03-14 20:41:26 +0000

    Added Sign-Up & Login Fields

--- application/views/login.php
+++ application/views/login.php
@@ -1,3 +1,14 @@
-<div class="container bg-primary text-center rounded my-3">
+<div>
+	<div class="container bg-primary text-center rounded my-3">
 		<h1>Login</h1>
 	</div>
+
+	<div>
+		<form action="<?php echo base_url(); ?>index.php/Login" method="POST">
+			<input type="text" placeholder="Username" name="" id="" />
+			<input type="text" placeholder="Password" name="" id="" />
+
+			<input type="submit" value="Login" />
+		</form>
+	</div>
+</div>
--- application/views/register.php
+++ application/views/register.php
@@ -1,3 +1,19 @@
-<div class="container bg-primary text-center rounded my-3">
+<div>
+	<div class="container bg-primary text-center rounded my-3">
 		<h1>Sign Up</h1>
 	</div>
+
+	<div>
+		<form action="<?php echo base_url(); ?>index.php/Register" method="POST">
+			<input type="text" placeholder="First Name:" name="" id="" />
+			<input type="text" placeholder="Surname" name="" id="" />
+			<input type="text" placeholder="Address" name="" id="" />
+			<input type="text" placeholder="Telephone" name="" id="" />
+			<input type="text" placeholder="Email" name="" id="" />
+			<input type="text" placeholder="Username" name="" id="" />
+			<input type="text" placeholder="Password" name="" id="" />
+
+			<input type="submit" value="Register" />
+		</form>
+	</div>
+</div>
