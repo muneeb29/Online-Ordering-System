7bde048c55a99d5fb8d9186c64f5b74b21eaba96
muneeb29 <98808077+muneeb29@users.noreply.github.com>
2022-03-16 16:45:58 +0000

    Modified Login & Sign-Up Form Front-End

--- application/views/login.php
+++ application/views/login.php
@@ -3,12 +3,47 @@
 		<h1>Login</h1>
 	</div>
 
-	<div>
-		<form action="<?php echo base_url(); ?>index.php/Login/login" method="POST">
-			<input type="text" placeholder="Username" name="username" id="username" />
-			<input type="text" placeholder="Password" name="password" id="password" />
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-5 mx-auto">
+				<div class="border-1 shadow rounded my-5">
+					<div class="p-5">
+				<div class="text-center">
+					<h4 class="mb-4 fs-4">Login</h4>
+				</div>
+				<form
+					action="<?php echo base_url(); ?>index.php/Login/login"
+					method="POST"
+				>
+				<div class="form-floating mb-3">
+					<input
+						type="text"
+						class="form-control"
+						placeholder="Username"
+						name="username"
+						/>
+						<label for="username">Username: </label/>
+				</div>
+				<div class="form-floating mb-3">
+					<input
+						type="password"
+						class="form-control"
+						placeholder="Password"
+						name="password"
+					/>
+					<label for="password">Password: </label/>
+					</div>
 
-			<input type="submit" value="Login" />
-		</form>
+					<div class="d-grid">
+					<input type="submit" value="Login" class="btn btn-primary fw-bold" />
+				</div>
+				</form>
+				</div>
+			</div>
+		</div>
+			</div>
+		</div>
 	</div>
-</div>
+
+
+
--- application/views/register.php
+++ application/views/register.php
@@ -3,17 +3,95 @@
 		<h1>Sign Up</h1>
 	</div>
 
-	<div>
-		<form action="register" method="POST">
-			<input type="text" placeholder="First Name:" name="firstname" id="firstname" />
-			<input type="text" placeholder="Surname" name="surname" id="surname" />
-			<input type="text" placeholder="Address" name="address" id="address" />
-			<input type="text" placeholder="Telephone" name="telephone" id="telephone" />
-			<input type="text" placeholder="Email" name="email" id="email" />
-			<input type="text" placeholder="Username" name="username" id="username" />
-			<input type="text" placeholder="Password" name="password" id="password" />
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-5 mx-auto">
+				<div class="border-1 shadow rounded my-5">
+					<div class="p-5">
+				<div class="text-center">
+					<h4 class="mb-4 fs-4">Sign Up</h4>
+				</div>
+				<form
+					action="register"
+					method="POST"
+				>
+				<div class="form-floating mb-3">
+					<input
+						type="text"
+						class="form-control"
+						placeholder="firstname"
+						name="firstname"
+						/>
+						<label for="firstname">Firstname: </label/>
+				</div>
 
-			<input type="submit" value="Register" name="submit"/>
-		</form>
-	</div>
+				<div class="form-floating mb-3">
+					<input
+						type="text"
+						class="form-control"
+						placeholder="surname"
+						name="surname"
+						/>
+						<label for="surname">Surname: </label/>
+				</div>
+
+
+				<div class="form-floating mb-3">
+					<input
+						type="text"
+						class="form-control"
+						placeholder="address"
+						name="address"
+						/>
+						<label for="address">Address: </label/>
+				</div>
+
+				<div class="form-floating mb-3">
+					<input
+						type="text"
+						class="form-control"
+						placeholder="telephone"
+						name="telephone"
+						/>
+						<label for="telephone">Telephone: </label/>
+				</div>
+
+				<div class="form-floating mb-3">
+					<input
+						type="text"
+						class="form-control"
+						placeholder="email"
+						name="email"
+						/>
+						<label for="email">Email: </label/>
+				</div>
+
+				<div class="form-floating mb-3">
+					<input
+						type="text"
+						class="form-control"
+						placeholder="Username"
+						name="username"
+						/>
+						<label for="username">Username: </label/>
+				</div>
+				<div class="form-floating mb-3">
+					<input
+						type="password"
+						class="form-control"
+						placeholder="Password"
+						name="password"
+					/>
+					<label for="password">Password: </label/>
+					</div>
+
+					<div class="d-grid">
+					<input type="submit" value="Register" name="submit" class="btn btn-primary fw-bold" />
+				</div>
+				</form>
+				</div>
+			</div>
+		</div>
+			</div>
+		</div>
 </div>
