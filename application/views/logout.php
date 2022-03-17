fb13db9dee78192fe66234550801b2f08657f46a
muneeb29 <98808077+muneeb29@users.noreply.github.com>
2022-03-17 17:04:14 +0000

    Updated Logout Page

--- application/views/logout.php
+++ application/views/logout.php
@@ -1,3 +1,21 @@
-<div class="container bg-white border-1 shadow rounded text-center my-4 py-1">
-		<h1>Logout</h1>
+<div>
+	<div class="container bg-white border-1 shadow rounded text-center my-4 py-1">
+		<h1>Logged Out</h1>
 	</div>
+
+	<div class="text-center">
+		<h3 class="mb-5">You Have Been Logged Out</h3>
+		<h5 class="mb-5">Thank You For Using Online Ordering System</h5>
+
+		<div class="d-grid col-6 mx-auto mb-2">
+			<a
+				href="<?php echo base_url(); ?>index.php/Login"
+				role="button"
+				class="btn btn-primary mb-4"
+				>Login Again</a
+			>
+		</div>
+
+		<h4 class="mb-5">See You Later</h4>
+	</div>
+</div>
