6d3d8af7170e157b3d737eb7c8b4da3d93ab1059
muneeb29 <98808077+muneeb29@users.noreply.github.com>
2022-03-20 18:18:10 +0000

    Ordered Scripts & Stylesheets

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -1,14 +1,13 @@
 <html>
-<link rel="stylesheet" href="<?php echo base_url('application/styles/stylesheet.css')?>"/>
+
+	<head>
+
 	<link
 		href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
 		rel="stylesheet"
 	/>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-	
-		<script src="<?php echo base_url('application/scripts/ajax-functions.js');?>">
-			<script src="<?php echo base_url('application/scripts/js-script.js');?>">
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+	<link rel="stylesheet" href="<?php echo base_url('application/styles/stylesheet.css')?>"/>
+	</head>	
 
 	<body>
 		<div class="row">
@@ -113,4 +112,11 @@
 			</div>
 		</nav>
 	</body>
+
+	<footer>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+	<script src="<?php echo base_url('application/scripts/ajax-functions.js');?>"></script>
+
+	</footer>
 </html>
