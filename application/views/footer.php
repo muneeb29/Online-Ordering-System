1f5541ddf413f5c9cd99375492b663e7d726561e
muneeb29 <98808077+muneeb29@users.noreply.github.com>
2022-04-20 19:19:06 +0100

    Modified Footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,69 +1,65 @@
 <html>
 <div class="scroll text-end mb-3 my-2">
 		<button onclick="topFunction()"  class="btn btn-primary scroll-top" id="topButton">Go to top</button>
-		</div>
+</div>
 
-	<div class="container-fluid bg-dark text-white my-2">
-		<div class="row text-center">
-			<div class="col py-1">
-				<h3>Online Ordering System</h3>
-			</div>
-		</div>
-		<div class="row mx-auto">
-			<div class="col fw-bold">
-				<h5>Help</h5>
-			</div>
-			<div class="col-md-3 fw-bold">
-				<h5>Quick Links</h5>
-			</div>
-		</div>
+  <footer class="bg-dark text-center text-lg-start text-white">
+    <div class="container p-3">
 
-		<div class="row mx-auto">
-			<div class="col text-start">
-				<ul>
-					<li>Privacy Policy</li>
-				</ul>
-			</div>
-			<div class="col col-md-3">
-				<a href="<?php echo base_url(); ?>index.php/Home">Home</a>
-			</div>
-		</div>
+	<div class="row text-center">
+	<div class="col">
+		<h2>Online Ordering System</h2>
+	</div>
+	</div>
 
-		<div class="row mx-auto">
-			<div class="col text-start">
-				<ul>
-					<li>Terms & Conditions</li>
-				</ul>
-			</div>
-			<div class="col col-md-3">
-				<a href="<?php echo base_url(); ?>index.php/Menu">Menu</a>
-			</div>
-		</div>
+      <div class="row">
+        <div class="col-lg-3 mb-4">
+          <h4 class="">Help</h4>
+          <ul class="list-unstyled mb-3">
+            <li>
+              <a href="#!" class="text-white mx-2">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white mx-2">Terms & Conditions</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white mx-2">Returns Information</a>
+            </li>
+			<li>
+              <a href="#!" class="text-white mx-2">Delivery Information</a>
+            </li>
+          </ul>
+        </div>
 
-		<div class="row mx-auto">
-			<div class="col text-start">
-				<ul>
-					<li>Returns Information</li>
-				</ul>
-			</div>
-			<div class="col col-md-3">
-				<a href="<?php echo base_url(); ?>index.php/Contact">Contact Us</a>
-			</div>
-		</div>
+        <div class="col-lg-3">
+        </div>
+        <div class="col-lg-3">
+        </div>
 
-		<div class="row mx-auto">
-			<div class="col text-start">
-				<ul>
-					<li>Delivery Information</li>
-				</ul>
-			</div>
-			<div class="col col-md-3">
-				<a href="<?php echo base_url(); ?>index.php/Home">Account</a>
-			</div>
-		</div>
+        <div class="col-lg-3">
+          <h4 class="">Quick Links</h4>
+          <ul class="list-unstyled">
+            <li>
+              <a href="<?php echo base_url(); ?>index.php/Home" class="text-white mx-2">Home</a>
+            </li>
+            <li>
+              <a href="<?php echo base_url(); ?>index.php/Menu" class="text-white mx-2">Menu</a>
+            </li>
+            <li>
+              <a href="<?php echo base_url(); ?>index.php/Contact" class="text-white mx-2">Contact Us</a>
+            </li>
+			<li>
+              <a href="<?php echo base_url(); ?>index.php/Login" class="text-white mx-2">Login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
 
-		<div class="row">
-			<div class="col text-center">Copyright © 2022</div>
-		</div>
+    <div class="text-center p-2" style="background-color: rgba(0, 0, 0, 0.3)">
+      <p class="text-white">© 2022 Copyright - Online Ordering System</p>
+    </div>
+  </footer>
+
 	</div>
 </html>
