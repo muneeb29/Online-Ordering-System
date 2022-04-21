da0c108d73b59414f3daa3e10073d1f36ecf0b4b
muneeb29 <98808077+muneeb29@users.noreply.github.com>
2022-04-21 19:47:13 +0100

    Modified Contact Page

--- application/views/contact.php
+++ application/views/contact.php
@@ -1,69 +1,99 @@
 <html>
-	<div>		
-		<div class="container bg-white border-1 shadow rounded text-center my-4 py-1">
+	<div>
+		<div
+			class="container bg-white border-1 shadow rounded text-center my-4 py-1"
+		>
 			<h1>Contact Us</h1>
 		</div>
 
-		<div class="container bg-light">
-			<div class="row text-center ">
-				<div class="col border bg-primary text-white rounded">
-					<h4>Faq's</h4>
+		<div class="container">
+			<div class="row text-center">
+				<div class="col border rounded mb-4 mx-4">
+					<h3 class="border shadow">Faq's</h3>
+					<div class="table-responsive">
+						<table
+							class="table align-middle mx-auto"
+							id="table"
+							style="width: 60%"
+						>
+							<tbody>
+								<tr>
+									<th>How Far Do We Deliver?</th>
+									<td>Within 5 Miles Radius</td>
+								</tr>
+								<tr>
+									<th>How Many Days A Week Are We Open?</th>
+									<td>7 Days A Week</td>
+								</tr>
+
+								<tr>
+									<th>Where Is the Restaurant Close To?</th>
+									<td>Close To The Town Centre</td>
+								</tr>
+
+								<tr>
+									<th>Is There A Car Park?</th>
+									<td>Yes We Offer Free Parking</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
 				</div>
-				<div class="col col-sm-1"></div>
-				<div class="col border bg-primary text-white rounded">
-					<h4>Contact</h4>
-				</div>
-			</div>
+				<div class="col-1"></div>
+		
 
-			<div class="row text-center">
-				<div class="col border border-dark fw-bold ">How Far Do We Deliver?</div>
-				<div class="col border border-dark">Within 5 Miles Radius</div>
-				<div class="col col-sm-1"></div>
-				<div class="col border border-dark fw-bold">Phone:</div>
-				<div class="col border border-dark">0161-000-0000</div>
-			</div>
+				<div class="col border rounded mb-4 mx-4">
+					<h3 class="border shadow">Contact</h3>
+					<div class="table-responsive">
+						<table
+							class="table align-middle mx-auto"
+							id="table"
+							style="width: 60%"
+						>
+							<tbody>
+								<tr>
+									<th>Phone</th>
+									<td>0161-000-0000</td>
+								</tr>
+								<tr>
+									<th>Address</th>
+									<td>
+										1 Restaurant Road <br />
+										1R0 5ET <br />
+										Restaurant City <br />
+										United Kingdom
+										<br />
+									</td>
+								</tr>
 
-			<div class="row text-center">
-				<div class="col border border-dark fw-bold">How Many Days A Week Are We Open?</div>
-				<div class="col border border-dark">7 Days A Week</div>
-				<div class="col col-sm-1"></div>
-				<div class="col border border-dark fw-bold">Address</div>
-				<div class="col border border-dark">
-					1 Restaurant Road <br />
-					1R0 5ET <br />
-					Restaurant City <br />
-					United Kingdom
-					<br />
-				</div>
-			</div>
+								<tr>
+									<th>Email</th>
+									<td>order@abc.com</td>
+								</tr>
 
-			<div class="row text-center">
-				<div class="col border border-dark fw-bold">Where Is the Restaurant Close To?</div>
-				<div class="col border border-dark">Close To The Town Centre</div>
-				<div class="col col-sm-1"></div>
-				<div class="col border border-dark fw-bold">Email</div>
-				<div class="col border border-dark">order@abc.com</div>
-			</div>
-
-			<div class="row text-center">
-				<div class="col border border-dark fw-bold">Is There A Car Park?</div>
-				<div class="col border border-dark">Yes We Offer Free Parking</div>
-				<div class="col col-sm-1"></div>
-				<div class="col border border-dark fw-bold">Opening Hours:</div>
-				<div class="col border border-dark">
-					Monday - 9:00 - 22:00
-					<br />
-					Tuesday - 9:00 - 22:00
-					<br />
-					Wednesday - 9:00 - 22:00
-					<br />
-					Thursday - 9:00 - 22:00
-					<br />
-					Friday - 9:00 - 22:00
-					<br />
-					Saturday - 9:00 - 23:00
-					<br />
-					Sunday - 11:30 - 19:00
+								<tr>
+									<th>Opening Hours:</th>
+									<td>
+										
+										Monday: 9:00 - 22:00
+										<br />
+										Tuesday: 9:00 - 22:00
+										<br />
+										Wednesday: 9:00 - 22:00
+										<br />
+										Thursday: 9:00 - 22:00
+										<br />
+										Friday: 9:00 - 22:00
+										<br />
+										Saturday: 9:00 - 23:00
+										<br />
+										Sunday: 11:30 - 19:00
+				
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
 				</div>
 			</div>
 		</div>
